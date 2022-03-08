93ca0c734da38cbe06fea816cd33b6d671c6916f
jimin <programmer0415@gmail.com>
2022-03-08 12:42:56 +0900

    Feat : 기능 추가됨

---
+++ src/main/java/com/example/Test/TestFeature.java
@@ -0,0 +1,18 @@
+package com.example.Test;
+
+public class TestFeature {
+    public int value1;
+    public int value2;
+
+    public TestFeature()
+    {
+        value2 =20;
+        value1 =30;
+
+    }
+
+    public int Generate()
+    {
+        return 0;
+    }
+}
