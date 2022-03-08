57cc3f94c90877e8c059e8978ab2da5944c7910f
programmer0415@gmail.com <jimin0415kr@snowpipe.co.kr>
2022-03-08 12:42:56 +0900

    Feat : 기능 추가됨

---
+++ src/main/java/com/example/Test/TestFeature.java
@@ -0,0 +1,16 @@
+package com.example.Test;
+
+public class TestFeature {
+    public int value1;
+    public int value2;
+
+    public TestFeature()
+    {
+
+    }
+
+    public int Generate()
+    {
+        return 0;
+    }
+}
